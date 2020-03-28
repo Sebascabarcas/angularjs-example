57a5281a2c9698db58afd495db8ee422cea8939d
Sebastian Cabarcas <sebascabarcas@gmail.com>
2020-03-27 20:11:25 -0400

    example

--- app/app.js
+++ app/app.js
@@ -3,12 +3,70 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  'ui.router',
+  'ngMaterial',
+  // 'myApp.view1',
+  // 'myApp.view2',
   'myApp.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  config(function ($stateProvider, $locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    // $routeProvider.otherwise({ redirectTo: '/login' });
+    // https://invimarefactory.desarrollodeprueba.com/admin/login
+    // {"nit":"admin@tvip.com","email":"admin@tvip.com","password":"123456"}
+    // $http({
+    //   method: 'GET',
+    //   url: '/someUrl'
+    // }).then(function successCallback(response) {
+    //     // this callback will be called asynchronously
+    //     // when the response is available
+    //   }, function errorCallback(response) {
+    //     // called asynchronously if an error occurs
+    //     // or server returns response with an error status.
+    //   });
+
+    var loginState = {
+      name: 'login',
+      url: '/login',
+      controller: 'LoginCtrl',
+      templateUrl: './sign-in/login.html',
+      // resolve: {
+      // authentication: async ($q, $http) => {
+      // try {
+      //     const a = $http({
+      //       method: 'POST',
+      //       url: 'https://invimarefactory.desarrollodeprueba.com/admin/login',
+      //       data: {"nit":"admin@tvip.com","email":"admin@tvip.com","password":"123456"} 
+      //     });
+      //   console.log(a)
+      // } catch (error) {
+      //   console.log(error)
+      // }
+      // .then(function successCallback(response) {
+      //     console.log(response);
+      //     // this callback will be called asynchronously
+      //     // when the response is available
+      //   }, function errorCallback(error) {
+      //     console.log(error)
+      //     // called asynchronously if an error occurs
+      //     // or server returns response with an error status.
+      //   });
+      //   var d = $q.defer();
+      //   console.log(d)
+      //   d.resolve(a)
+      // d.reject();
+      //   return d.promise;
+      // }
+      // }
+    }
+
+    var homeState = {
+      name: 'home',
+      url: '/home',
+      templateUrl: './home/index.html'
+    }
+
+    $stateProvider.state(loginState);
+    $stateProvider.state(homeState);
+  });
